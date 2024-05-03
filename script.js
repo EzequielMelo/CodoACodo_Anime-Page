7221f1a1be5e2445c8921f9729b536bf08647724
JpCortezF <juanpactz@gmail.com>
2024-05-03 14:04:47 -0300

    Se mejoran detalles en la página principal.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    const maxLength = 300;
+    const maxLength = 350;
     const descriptions = document.querySelectorAll('.text-limit');
     descriptions.forEach(description => {
         const text = description.innerHTML;
-        if (text.length > maxLength) {
-            const truncatedText = text.substring(0, maxLength) + '...';
+        if (text.length > maxLength){
+            let truncatedText = text.substring(0, maxLength);
+            if (truncatedText.charAt(truncatedText.length - 1) === ' ') {
+                truncatedText += '...';
+            } else {
+                const lastSpaceIndex = truncatedText.lastIndexOf(' ');
+                truncatedText = truncatedText.substring(0, lastSpaceIndex) + '...';
+            }
             description.innerHTML = truncatedText;
         }
     });
