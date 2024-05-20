8c33805b7bec29c16fbd09964155480af888e1eb
EzequielMelo <ezequielanthonymelo@gmail.com>
2024-05-20 18:57:38 -0300

    Varios cambios ahora en la seccion de empleo se añadieron validaciones al poner datos y tambien se hicieron otros cambios de diseño como en el footer donde ahora aparecen los nombres de los colaboradores y el repositorio.

--- script.js
+++ script.js
@@ -41,5 +41,73 @@ document.addEventListener("DOMContentLoaded", function() {
             forgotID.innerHTML = "";
         }
     }
+
+    jQuery.validator.addMethod("exactlength", function(value, element, param) {
+        return this.optional(element) || value.length == param;
+    }, $.validator.format("Por favor ingrese {0} characteres."));
+
+    $("#form1").validate({
+        rules: {
+            nombre: {
+                required: true,
+                minlength: 3,
+                maxlength: 30,
+            },
+            apellido: {
+                required: true,
+                minlength: 3,
+                maxlength: 30,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefono: {
+                required: true,
+                number: true
+            },
+            pais: {
+                required: true,
+                minlength: 3,
+                maxlength: 30
+            },
+            terminos: {
+                required: true,
+                maxlength: 1
+            }
+        }
+    });
+
+    $(".btn-aplicar").click(function(){
+        if($("#form1").valid() == false)
+        {
+            return;
+        }
+        let email = $("#email").val();
+        let nombre = $("#nombre").val();
+        let apellido = $("#apellido").val();
+        let telefono =$("#telefono").val();
+        let pais = $("#pais").val();
+    });
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "Este campo es obligatorio.",
+        remote: "Por favor, rellena este campo.",
+        email: "Por favor, escribe una dirección de correo válida",
+        url: "Por favor, escribe una URL válida.",
+        date: "Por favor, escribe una fecha válida.",
+        dateISO: "Por favor, escribe una fecha (ISO) válida.",
+        number: "Por favor, escribe un número entero válido.",
+        digits: "Por favor, escribe sólo dígitos.",
+        creditcard: "Por favor, escribe un número de tarjeta válido.",
+        equalTo: "Por favor, escribe el mismo valor de nuevo.",
+        accept: "Por favor, escribe un valor con una extensión aceptada.",
+        maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
+        minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
+        rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+        range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
+        max: jQuery.validator.format("Por favor, escribe un valor menor o igual a {0}."),
+        min: jQuery.validator.format("Por favor, escribe un valor mayor o igual a {0}.")
+    });
 });
 
