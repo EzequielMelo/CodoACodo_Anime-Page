4031e3f89beb1bd2bda4e5a6df9aa0651e13e176
EzequielMelo <ezequielanthonymelo@gmail.com>
2024-05-21 01:21:47 -0300

    pequeño cambio al mensaje de caracteres minimos en la validacion

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ document.addEventListener("DOMContentLoaded", function() {
         equalTo: "Por favor, escribe el mismo valor de nuevo.",
         accept: "Por favor, escribe un valor con una extensión aceptada.",
         maxlength: jQuery.validator.format("Por favor, no escribas más de {0} caracteres."),
-        minlength: jQuery.validator.format("Por favor, no escribas menos de {0} caracteres."),
+        minlength: jQuery.validator.format("Por favor, escriba mas de {0} caracteres."),
         rangelength: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
         range: jQuery.validator.format("Por favor, escribe un valor entre {0} y {1}."),
         max: jQuery.validator.format("Por favor, escribe un valor menor o igual a {0}."),
